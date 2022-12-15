9f7b8a7582043a62c4b6d255600234881d9fe72d
JunHyeon <dlwnsgus07@naver.com>
2022-12-15 23:05:15 +0900

    feat : 제너레이터를 통해 잔액의 동전개수를 만드는 기능 추가

---
+++ src/main/java/domain/Change.java
@@ -0,0 +1,10 @@
+package domain;
+
+import java.util.HashMap;
+
+public class Change {
+    private HashMap<Integer, Integer> changeNumber;
+    public Change(HashMap<Integer, Integer> coinNumber) {
+        this.changeNumber = coinNumber;
+    }
+}
