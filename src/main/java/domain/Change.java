56403cdab456ea75a9b587462b6bb28b6c309ad2
JunHyeon <dlwnsgus07@naver.com>
2022-12-16 00:38:27 +0900

    feat : 가장 최소의 잔돈개수를 계산하는 기능 추가

--- src/main/java/domain/Change.java
+++ src/main/java/domain/Change.java
@@ -1,10 +1,32 @@
 package domain;
 
+import vendingmachine.Coin;
+
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Change {
     private HashMap<Integer, Integer> changeNumber;
+
     public Change(HashMap<Integer, Integer> coinNumber) {
         this.changeNumber = coinNumber;
+    }
+
+    public HashMap<Integer, Integer> CalcMinimumChangeNumber(int amount) {
+        HashMap<Integer, Integer> minimumChange = new HashMap<>();
+        Arrays.stream(Coin.values()).forEach(coin -> {
+            int maxNumber = 0;
+            while (amount != 0) {
+                if (amount - coin.getAmount() * maxNumber >= 0) {
+                    maxNumber += 1;
+                } else {
+                    break;
+                }
+                if (maxNumber != 0) {
+                    minimumChange.put(coin.getAmount(), maxNumber);
+                }
+            }
+        });
+        return minimumChange;
     }
 }
